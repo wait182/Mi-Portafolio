a0bdbd48952f67782f9e39c2411a2f994298f451
David Chacon <dchacon017@gmail.com>
2020-01-08 17:15:32 -0300

    four commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
-$(function(){  
+$(function(){
+
+  $('[data-toggle="popover"]').popover();
+
+  $("a").click(function(event){
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var cat = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(cat).offset().top
+      }, 600, function(){
+        window.location.hash = cat;
+      });
+    }
+  });
 
 });
